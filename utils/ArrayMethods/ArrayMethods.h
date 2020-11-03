20aa350be33eb9d6017b9bc614c5315dbd170659
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2020-11-03 18:04:37 -0500

    added find closest array methods

--- utils/ArrayMethods/ArrayMethods.h
+++ utils/ArrayMethods/ArrayMethods.h
@@ -4,5 +4,9 @@
 int arr_max(int arr[], int n);
 int arr_min(int arr[], int n);
 int arr_average(int arr[], int n);
+int getClosestInt(int val1, int val2, int target);
+float getClosestFloat(float val1, float val2, float target);
+int arr_find_closest_int(int arr[], int n, int target);
+float arr_find_closest_float(float arr[], int n, float target);
 
 #endif
