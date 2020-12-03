63e64c09575164e65e5c76065f3cd3d5740116d9
Scott Campbell <scott.m.campbell11@gmail.com>
2020-12-03 12:34:33 -0500

    comments

--- scripts/VoltPerOctave16bitCalculator.py
+++ scripts/VoltPerOctave16bitCalculator.py
@@ -4,34 +4,27 @@
 
 resolution = 65535
 
-Vmin = float(input('Enter DAC min output voltage (after amplification): ') or "0")
-Vmax = float(input('Enter DAC max output voltage (after amplification): ') or "5.82")
+Vmin = float(input('Enter min DAC output voltage (after amplification): ') or "0")
+Vmax = float(input('Enter max DAC output voltage (after amplification): ') or "5.82")
 Vfloor = float(input('Enter voltage floor (before calibration): ') or "0.5")
-
-# numBits = int(input('DAC bit resolution: '))
-
-
 
 numNotes = 12
 numOctaves = 6
 
-bitVoltage = (Vmax - Vmin) / resolution
+voltPerBit = (Vmax - Vmin) / resolution #  the change in voltage from ex. 22 to 23 == 0.00008880750744 volts
 
-floor16bit = Vfloor / bitVoltage
-range16Bit = 1 / bitVoltage
-
+floor16bit = Vfloor / voltPerBit
+range16Bit = 1 / voltPerBit
 print('16 bit floor: ', floor16bit)
 print('1 Volt Range (ie. 0.5v to 1.5v): ', range16Bit)
 
-
-# take the floor + 1 volt range 16 bit numbers and map them to a 12 tone scale across 5 octaves
 
 halfStep = range16Bit / numNotes
 print('half step 16 bit value: ', halfStep)
 
 VOLTAGE_MAP = []
 
-for x in range(64):
+for x in range( numNotes * numOctaves ):
     VOLTAGE_MAP.append(int(floor16bit + (halfStep*x)))
 
 print(VOLTAGE_MAP) # 16,890.46
