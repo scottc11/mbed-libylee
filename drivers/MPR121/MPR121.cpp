b958db0251a9b25dce4ca8674e21c94714f9141b
Scott Campbell <scott.m.campbell11@gmail.com>
2021-02-09 08:47:25 -0500

    comments

--- drivers/MPR121/MPR121.cpp
+++ drivers/MPR121/MPR121.cpp
@@ -1,24 +1,4 @@
-/**
- * @file    MPR121.cpp
- * @brief   Device driver - MPR121 capactiive touch IC
- * @author  sam grove
- * @version 1.0
- * @see     http://cache.freescale.com/files/sensors/doc/data_sheet/MPR121.pdf
- *
- * Copyright (c) 2013
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 #include "MPR121.h"
 #include "mbed_debug.h"
@@ -180,6 +160,10 @@ void MPR121::clearInterupt() {
     return;
 }
 
+/**
+ * @brief Attach a callback to be invoked when an interupt is detected. MUST be ISR safe
+ * @param func MBED Callback function
+*/
 void MPR121::attachInteruptCallback(Callback<void()> func) {
     interuptCallback = func;
 }
