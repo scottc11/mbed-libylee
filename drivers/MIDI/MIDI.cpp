49513cdb6b6d3fcbd12968897dac6ef26a32e6c9
Scott Campbell <scott.m.campbell11@gmail.com>
2021-05-25 16:30:06 -0400

    update to mbed version 6.11.0

--- drivers/MIDI/MIDI.cpp
+++ drivers/MIDI/MIDI.cpp
@@ -7,18 +7,15 @@ void MIDI::sendNoteOn(uint8_t _channel, uint8_t _note, uint8_t _velocity) {
   uint8_t status = 0x90 | _channel; // byte 1 -> midi function
   uint8_t note = _note;             // byte 2 -> note (0 - 127)
   uint8_t velocity = _velocity;     // byte 3 -> velocity (default to 64)
+  char buff[3] = {status, note, velocity};
 
-  serial.putc(status);
-  serial.putc(note);
-  serial.putc(velocity);
+  serial.write(buff, 3);
 }
 
 void MIDI::sendNoteOff(uint8_t _channel, uint8_t _note, uint8_t _velocity) {
   uint8_t status = 0x80 | _channel;
   uint8_t note = _note;
   uint8_t velocity = _velocity;
-
-  serial.putc(status);
-  serial.putc(note);
-  serial.putc(velocity);
+  char buff[3] = {status, note, velocity};
+  serial.write(buff, 3);
 }
