8da1e78dc9fa36d6f5a83103a56c9f39054d48ee
scottc11 <scott.m.campbell11@gmail.com>
2022-01-29 14:58:58 -0500

    map_num_in_range template

--- utils/Algorithms/Algorithms.h
+++ utils/Algorithms/Algorithms.h
@@ -2,8 +2,14 @@
 #define __OK_ALGORITHMS_H
 
 int invertIntInRange(int min, int max, int num);
-int scaleIntToRange(int input, int minInput, int maxInput, int minOutput, int maxOutput);
+
 int getClosestInt(int val1, int val2, int target);
 float getClosestFloat(float val1, float val2, float target);
+
+template <class T>
+T map_num_in_range(T input, T minInput, T maxInput, T minOutput, T maxOutput)
+{
+    return ((float)(maxOutput - minOutput) / (maxInput - minInput)) * (input - minInput);
+}
 
 #endif
