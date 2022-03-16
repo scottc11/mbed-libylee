26d22a3efa341937bc587aefffba1a22e967a53a
scottc11 <scott.m.campbell11@gmail.com>
2022-03-16 15:18:39 -0400

    tweaked calculator for better input/output

--- scripts/VoltPerOctave16bitCalculator.py
+++ scripts/VoltPerOctave16bitCalculator.py
@@ -7,9 +7,15 @@ resolution = 65535
 Vmin = float(input('Enter min DAC output voltage (after amplification): ') or "0")
 Vmax = float(input('Enter max DAC output voltage (after amplification): ') or "5.82")
 Vfloor = float(input('Enter voltage floor (before calibration): ') or "0.5")
+numOctaves = int(input('Enter number of OCTAVES: ') or "6")
+invertOutput = input("Will the output be inverted (ie. the higher the value the lower the voltage output)? Enter 'true' or 'false': ")
+
+if invertOutput == "true" or invertOutput == "True":
+    invertOutput = True
+elif invertOutput == "false" or invertOutput == "False":
+    invertOutput = False
 
 numNotes = 12
-numOctaves = 6
 
 voltPerBit = (Vmax - Vmin) / resolution #  the change in voltage from ex. 22 to 23 == 0.00008880750744 volts
 
@@ -20,11 +26,15 @@ print('1 Volt Range (ie. 0.5v to 1.5v): ', range16Bit)
 
 
 halfStep = range16Bit / numNotes
-print('half step 16 bit value: ', halfStep)
+print('Half-step 16 bit value: ', halfStep)
 
 VOLTAGE_MAP = []
 
 for x in range( numNotes * numOctaves ):
     VOLTAGE_MAP.append(int(floor16bit + (halfStep*x)))
 
+if invertOutput:
+    VOLTAGE_MAP = list(reversed(VOLTAGE_MAP))
+
+print("Array Length: ", len(VOLTAGE_MAP))
 print(VOLTAGE_MAP) # 16,890.46
