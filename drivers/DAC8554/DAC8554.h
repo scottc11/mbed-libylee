cfc4b9a098acb122ef0513bfa4191924c3650085
scottc11 <scott.m.campbell11@gmail.com>
2022-03-31 18:47:14 -0400

    created cpp file for dac

--- drivers/DAC8554/DAC8554.h
+++ drivers/DAC8554/DAC8554.h
@@ -37,15 +37,8 @@ public:
     // spi.frequency(25000000); // 25MHz
   }
   
-  void init() {
-    spi.init();
-  };
-  
-  // 485 and 64741
-  void write(DAC8554::Channel chan, uint16_t value, uint8_t mode = DAC8554_SINGLE_WRITE) {
-    uint8_t config = mode | (chan << 1);
-    this->writeRegister(config, value);
-  };
+  void init();
+  void write(DAC8554::Channel chan, uint16_t value, uint8_t mode = DAC8554_SINGLE_WRITE);
 
 private:
   
